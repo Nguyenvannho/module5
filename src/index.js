a213a92da4639c2fc13b27285f322a9d5fbc15bc
Nguyenvannho <nho38443@gmail.com>
2023-03-30 13:40:19 +0700

    bai tap calculato may tinh

--- src/index.js
+++ src/index.js
@@ -2,26 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import PropTypes from 'prop-types';
-import Alert from './components/Alert';
+// import Alert from './components/Alert';
+import Calculator from './Calculator';
 
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Calculator />
   </React.StrictMode>
 );
 
-function App() {
-  return (
-    <div className="App">
-      <h3>Alert
-      <Alert text= "Cảnh báo! Tài nguyên bạn vừa truy cập không tồn tại." />
-      </h3>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <h3>Alert
+//       <Alert text= "Cảnh báo! Tài nguyên bạn vừa truy cập không tồn tại." />
+//       </h3>
+//     </div>
+//   );
+// }
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
